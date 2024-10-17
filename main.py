31ec3c39309e8d258fbc5427a40201bc4488f531
AliciaC-git <Aliciachuaxw@gmail.com>
2024-10-17 04:44:47 +0000

    update the main.py

--- main.py
+++ main.py
@@ -1,32 +1,9 @@
-import google.generativeai as genai
-import PIL.Image
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-genai.configure(api_key = st.secrets('GOOGLE_API_KEY'))
-
-def foodAnalyzing(Type, Age, Breed, Mood, Health_Condition):
-    st.title("Animal food analyzer")
-    uploaded_file = st.file_uploader("Upload food image", type=["jpg", "jpeg", "png"])
-    
-    if uploaded_file is not None:
-        # Open the image using PIL
-        image = PIL.Image.open(uploaded_file)
-
-        model = genai.GenerativeModel(
-            "gemini-1.5-flash",
-            system_instruction="""
-            You are an animal food analyzer. 
-            You will first analyze the food inside the uploaded image.
-            Then analyze whether the specific animal can eat the food or not.
-            You will list all the food in the image. The output will be in the format as shown below:
-            Food in the image: <food>
-
-            Analysis: <analysis of the can and cannot eat>
-            """
-        )
-
-        response = model.generate_content(["Identify whether the animal can eat the food.", image])
-        st.subheader("Identification Result")
-        st.text(response.text)
-
-foodAnalyzing("Dog", "2", "Chihuahua", "Happy", "None")
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=["lat", "lon"],
+)
+st.map(df)
